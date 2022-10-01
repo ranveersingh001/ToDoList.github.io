e623084d77356077d699a2e56a5cda8fd8446337
ranveersingh001 <ranveerjat777@gmail.com>
2022-10-01 13:45:31 +0530

    add to do list

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ToDoList.github.io
+
+# Can Create a To DO List .
+
+# Take input from user.
+
+# click on + (Add) Button it will create a list and add item to do list.
+
+# On click item can do line through.
+
+# on double click item will be removed.
--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+// Take 3 variables
 let addToDButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
  let inputField =document.getElementById('inputField');
 
+ // Function to create a To do list 
  addToDButton.addEventListener('click', function(){
+    // create list item
     var paragraph = document.createElement('li');
+    // add css to list
     paragraph.classList.add('paragraph-styling');
+    // input value from user
     paragraph.innerText = inputField.value;
+    // add list item 
     toDoContainer.appendChild(paragraph);
+    // after add item input will be blank
     inputField.value = "";
+    //on click item line-through
     paragraph.addEventListener('click', function(){
-    paragraph.style.textDecoration = "line-through";
-     
+    paragraph.style.textDecoration = "line-through";     
     })
+    // on double click item will remove item from list
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph)
     })
